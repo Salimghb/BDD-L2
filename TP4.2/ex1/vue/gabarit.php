657c5ee36c4ca859e15e56c4d1acb7110bbe9c30
Salimghb <sghbabra@hotmail.fr>
2017-01-28 05:53:35 +0100

    j
    tyjt

--- TP4.2/ex1/vue/gabarit.php
+++ TP4.2/ex1/vue/gabarit.php
@@ -7,26 +7,9 @@
 	  
     </head>
   <body>
-		<form id="monForm1" method="post" action="forum.php">
-			<fieldset>
-				<legend>Poster un message</legend>
-				<p>
-					<label for="login">Login</label>
-					<input type="text" name ="login" id="login" />
-				</p>
-				<p>
-					<label for="mdp">Mot de passe</label>
-					<input type="password" name ="pass" id="pass" />
-				</p>
-				<p>
-					<label for="message">Message</label>
-					<input type="text" name="message" id="message" />
-				</p>
-				<p>
-					<input type="Submit" value="Envoyer votre message" name="envoyer">
-				</p>
-			</fieldset>
-		</form>
-		<?php echo $contenu ; ?>
+ 
+  
+
+
 </body>
 </html>
